ffa460ebb684f61a1b9482ffe8db8d93865efb2c
Hamza Gamal <54688005+hamza94max@users.noreply.github.com>
2023-01-08 15:43:58 +0200

    initial commit : Setup project

---
+++ app/src/main/java/com/hamza/Foody/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.hamza.Foody.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.hamza.Foody.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
