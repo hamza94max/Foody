c4d6f6c555040e2d47e722d52fdfb561376b53ea
Hamza Gamal <54688005+hamza94max@users.noreply.github.com>
2023-01-08 15:44:22 +0200

    initial commit : Setup project

--- app/src/main/java/com/hamza/Foody/ui/HomeFragment/HomeFragment.kt
+++ app/src/main/java/com/hamza/Foody/ui/HomeFragment/HomeFragment.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.hamza.Foody.databinding.FragmentHomeBinding
+import com.hamza.directcalling.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
