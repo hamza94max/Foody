e485c866ba23e0daeb2df311d180fca83317556e
Hamza Gamal <54688005+hamza94max@users.noreply.github.com>
2023-07-02 17:47:58 +0300

    Home: handle searchView ✅

--- app/src/main/java/com/hamza/Foody/ui/HomeFragment/HomeFragment.kt
+++ app/src/main/java/com/hamza/Foody/ui/HomeFragment/HomeFragment.kt
@@ -4,16 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hamza.Foody.databinding.FragmentHomeBinding
+import com.hamza.domain.entity.Category
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
@@ -21,6 +22,7 @@ class HomeFragment : Fragment() {
 
     @Inject
     lateinit var homeAdapter: HomeAdapter
+
     private val mealsViewModel by viewModels<MealsViewModel>()
 
     override fun onCreateView(
@@ -37,25 +39,40 @@ class HomeFragment : Fragment() {
 
         mealsViewModel.getMeals()
 
-        lifecycleScope.launch {
-            observeToRemoteData()
-        }
+        lifecycleScope.launch { observeToMealsData() }
+
+        handleSearchView()
 
     }
 
-    private suspend fun observeToRemoteData() {
+    private suspend fun observeToMealsData() {
         mealsViewModel.categories.collect {
-            homeAdapter.differ.submitList(it?.categories)
-            setUpRecyclerView()
+            setUpRecyclerView(it?.categories!!)
         }
     }
 
-    private fun setUpRecyclerView() {
+    private fun setUpRecyclerView(categories: List<Category>) {
         binding.mealsRecyclerView.apply {
+            homeAdapter.differ.submitList(categories)
             layoutManager = GridLayoutManager(context, 2, LinearLayoutManager.VERTICAL, false)
             adapter = homeAdapter
         }
     }
 
+    private fun handleSearchView() {
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
 
+            override fun onQueryTextChange(newText: String): Boolean {
+                val filteredList = homeAdapter.differ.currentList.filter { category ->
+                    category.strCategory.contains(newText, ignoreCase = true)
+                }
+                homeAdapter.differ.submitList(filteredList)
+
+                return true
+            }
+        })
+    }
 }
